975758b78b981d4bd5125679e8eed19b79e3e84e
elmonitor <info@elmonitor.net>
2021-07-02 19:41:36 -0500

    Create __manifest__.py

---
+++ animal_medicine/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright (C) 2021 elmonitor.net
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Animal Medicine",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "summary": "Add the list of medicines for the animal",
+    "author": "elmonitor.net",
+    "maintainer": "elmonitor.net",
+    "website": "https://github.com/elmonitor/animal-addons",
+    "depends": ["animal"],
+    "data": ["views/animal.xml"],
+    "application": False,
+}
