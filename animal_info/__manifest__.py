9ebf3770d04b996eba1d09cb79257149f11c7197
elmonitor <info@elmonitor.net>
2021-07-22 10:00:59 -0500

    Create __manifest__.py

---
+++ animal_info/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright (C) 2021 elmonitor.net
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Animal Info",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "summary": "Add info to the animal",
+    "author": "elmonitor.net",
+    "maintainer": "elmonitor.net",
+    "website": "https://github.com/elmonitor",
+    "depends": ["animal"],
+    "data": ["views/animal.xml",
+    "application": False,
+}
